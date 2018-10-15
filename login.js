37e7efa46cd089f0db6548b01ca165eefcef498b
Chris Algier <chal18ac@student.cbs.dk>
2018-10-15 10:01:07 +0200

    login simple

---
+++ login.js
@@ -0,0 +1,28 @@
+var objUser = [
+    {
+        username: "Chris",
+        password: "123"
+    },
+    {
+        username: "Sophia",
+        password: "456"
+    },
+    {
+        username: "Filippo",
+        password: "789"
+    }
+]
+
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    for (var i = 0; i < objUser.length; i++) {
+        if (username == objUser[i].username && password == objUser[i].password) {
+            console.log(username + " is logged in")
+        } else {
+            console.log("Incorrect username or password")
+        }
+        return
+    }
+}
--- test.js
+++
@@ -1,3 +0,0 @@
-var concert = (Michael
-Jackson
-)
