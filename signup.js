92cba83593559c4c43f30bc9030d541c91806508
sophianagler <sona18ab@student.cbs.dk>
2018-10-17 12:37:02 +0200

    Form validation

--- signup.js
+++ signup.js
@@ -1,12 +1,50 @@
-//Sign up - Form validation
-//If a form field (fname) is empty, this function alerts a message, and returns false, to prevent the form from being submitted
+//Sign up - receive data from html put in
+function getInfo() {
+    var firstName = document.getElementById("firstName").value
+    var surName = document.getElementbyId("surName").value
+    var userName = document.getElementById("userName").value
+    var email = document.getElementbyId("email").value
+    var password = document.getElementById("password").value
+}
 
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
+//Sign up - Form validation
+//If a form field (name) is empty, this function alerts a message, and returns false, to prevent the form from being submitted
+
+function validateSignUp() {
+    var firstName = document.forms["SignUpForm"]["firstName"];
+    var surName = document.forms ["SignUpForm"]["lastName"];
+    var userName = document.forms ["SignUpForm"]["userName"];
+    var email = document.forms ["SignUpForm"]["email"];
+    var password = document.forms ["SignUpForm"]["password"];
+
+    if (firstName.value == "") {
+        alert("Please fill in your first name);
         return false;
     }
+    //regex to match only letters (case-insensitive)
+    //var reg = /^[a-z]+$/i;
+    //if(!reg.test(firstName.value))
+    // {
+    //   alert("Input contains invalid characters!");
+    //  return false;
+    // }
+    if (surName.value == "") {
+        alert("Please fill in your Surname");
+        return false;
+    }
+    if (userName.value == "") {
+        alert("Please fill in your Username");
+        return false;
+    }
+    if (email.value == "") {
+        alert("Please fill in your email");
+        return false;
+    }
+    if (password.value == "") {
+        alert("Please choose and fill in a password");
+        return false;
+    }
+    return true;
 }
 
 // Sign up - User object creation
