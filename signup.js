f54a79efa15f05d1d6cf17ee5eff014769d49b66
sophianagler <sona18ab@student.cbs.dk>
2018-10-17 20:22:30 +0200

    Include error message if fields are empty

--- signup.js
+++ signup.js
@@ -1,67 +1,62 @@
 //Sign up - receive data from html put in
+/*
 function getInfo() {
-    var firstName = document.getElementById("firstName").value
-    var surName = document.getElementbyId("surName").value
-    var userName = document.getElementById("userName").value
-    var email = document.getElementbyId("email").value
-    var password = document.getElementById("password").value
+    var firstName = document.getElementsByName("firstName").value;
+    var surName = document.getElementsByName("surName").value;
+    var userName = document.getElementsByName("userName").value;
+    var email = document.getElementsByName("email").value;
+    var password = document.getElementsByName("password").value;
 }
-
+*/
 //Sign up - Form validation
 //If a form field (name) is empty, this function alerts a message, and returns false, to prevent the form from being submitted
 
-function validateSignUp() {
-    var firstName = document.forms["SignUpForm"]["firstName"];
-    var surName = document.forms ["SignUpForm"]["lastName"];
-    var userName = document.forms ["SignUpForm"]["userName"];
-    var email = document.forms ["SignUpForm"]["email"];
-    var password = document.forms ["SignUpForm"]["password"];
 
-    if (firstName.value == "") {
-        alert("Please fill in your first name);
+function validateSignUp() {
+//reference to fields
+    var firstName = document.getElementById("firstName");
+    var surName = document.getElementById("surName");
+    var userName = document.getElementById("userName");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+//check empty form fields
+    if (firstName.value === "") {
+        alert("Please fill in your first name");
         return false;
     }
-    //regex to match only letters (case-insensitive)
-    //var reg = /^[a-z]+$/i;
-    //if(!reg.test(firstName.value))
-    // {
-    //   alert("Input contains invalid characters!");
-    //  return false;
-    // }
-    if (surName.value == "") {
+    if (surName.value === "") {
         alert("Please fill in your Surname");
         return false;
     }
-    if (userName.value == "") {
+    if (userName.value === "") {
         alert("Please fill in your Username");
         return false;
     }
-    if (email.value == "") {
+    if (email.value === "") {
         alert("Please fill in your email");
         return false;
     }
-    if (password.value == "") {
+    if (password.value === "") {
         alert("Please choose and fill in a password");
         return false;
+    }
+    if (inputAlphabet(firstName)) {
+        if (inputAlphabet(surName))
+            return true;
     }
     return true;
 }
 
-// Sign up - User object creation
-var myUser = new User("Sophia", "Nagler", "sophia21", "sophia@gmail.com", "alex");
+//function that checks whether input text is an alphabetic or not
 
-// objects are created with user as prototype -> have its properties
-var user1 = Object.create(user);
-user1.init("Sophia", "Nagler", "Sophia1993", "so.nagler@icloud.com", "Sophia1993");
 
-var user2 = Object.create(user);
-user2.init("Chris", "Algaier", "Chris123", "chris@icloud.com", "7649chris");
-// create array called "users" containing objects
-var users = [];
-users.push(myUser);
-users.push(user1);
-users.push(user2);
-//test output
-users.forEach(function (user) {
-    console.log(user.describe());
-});
+function inputAlphabet(inputtext) {
+    var alphaExp = /^[a-zA-Z]+$/;
+    if (inputtext.value.match(alphaExp)) {
+        return true;
+    } else {
+        alert("Please use alphabets only");
+        return false;
+    }
+
+}
