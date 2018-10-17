403f372b3dc843be3c59830470c08f96e65deebd
sophianagler <sona18ab@student.cbs.dk>
2018-10-17 09:46:16 +0200

    Creation of classes

--- signup.js
+++ signup.js
@@ -10,22 +10,8 @@ function validateForm() {
 }
 
 // Sign up - User object creation
-var user = {
-    // Initialise user
-    // init gives the User obkect the properties
-    init: function (surName, lastName, userName, eMail, password) {
-        this.surName = surName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.eMail = eMail;
-        this.password = password;
-    },
-    //describe function to test the output
-    describe: function () {
-        var description = this.surName + ", " + this.lastName;
-        return description;
-    }
-};
+var myUser = new User("Sophia", "Nagler", "sophia21", "sophia@gmail.com", "alex");
+
 // objects are created with user as prototype -> have its properties
 var user1 = Object.create(user);
 user1.init("Sophia", "Nagler", "Sophia1993", "so.nagler@icloud.com", "Sophia1993");
@@ -34,6 +20,7 @@ var user2 = Object.create(user);
 user2.init("Chris", "Algaier", "Chris123", "chris@icloud.com", "7649chris");
 // create array called "users" containing objects
 var users = [];
+users.push(myUser);
 users.push(user1);
 users.push(user2);
 //test output
