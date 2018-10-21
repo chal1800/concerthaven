95241bf6a34b675850aa97ca87bf40364c02991a
sophianagler <sona18ab@student.cbs.dk>
2018-10-21 16:35:58 +0200

    Update

---
+++ ideas.js
@@ -0,0 +1,32 @@
+/*      var inputs = [];
+       inputs.push (firstName, surName, userName, email, password);
+
+       for (var i = 0; i < inputs.length; i++){
+           if (inputs[i] === "") {
+               alert("Please fill in all the fields);
+               return false;
+           }
+       }
+   }
+*/
+// if true -> return data + create object + push in array
+// Sign up - User object creation
+/*
+var myUser = new User("Sophia", "Nagler", "sophia21", "sophia@gmail.com", "alex");
+
+// objects are created with user as prototype -> have its properties
+var user1 = Object.create(user);
+user1.init("Sophia", "Nagler", "Sophia1993", "so.nagler@icloud.com", "Sophia1993");
+
+var user2 = Object.create(user);
+user2.init("Chris", "Algaier", "Chris123", "chris@icloud.com", "7649chris");
+// create array called "users" containing objects
+var users = [];
+users.push(myUser);
+users.push(user1);
+users.push(user2);
+//test output
+users.forEach(function (user) {
+    console.log(user.describe());
+});
+*/
