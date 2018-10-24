cfc9750062cf15ffc42ef25e2888fd9cc50a31d3
Chris Algier <chal18ac@student.cbs.dk>
2018-10-24 13:21:54 +0200

    Geolocation Feature

--- geoloc.js
+++ geoloc.js
@@ -1,49 +1,28 @@
-/* function getLoc() {
-    var userL = document.getElementById("locSelect").value;
-    document.getElementById("demo").innerHTML = userL;
-    console.log (userL);
-}
-*/
-
 //Get coordinates from user input and create an object for the user's location
+var userLocation = {};
+
 function getCoords() {
     var userL = document.getElementById("locSelect").value;
     if ((userL === "Frederiksberg")) {
-        uLong = 55.676936;
-        uLat = 12.506579;
-        console.log(uLong, uLat)
+        userLocation.uLong = 55.676936;
+        userLocation.uLat = 12.506579;
 
     } else if ((userL === "Nørrebro")) {
-        uLong = 55.699031;
-        uLat = 12.556984;
-        console.log(uLong, uLat)
+        userLocation.uLong = 55.699031;
+        userLocation.uLat = 12.556984;
 
     } else if ((userL === "Vesterbro")) {
-        uLong = 55.664409;
-        uLat = 12.541514;
-        console.log(uLong, uLat);
+        userLocation.uLong = 55.664409;
+        userLocation.uLat = 12.541514;
 
     }
     document.getElementById("demo").innerHTML = userL;
-    console.log(userL);
-    userLocation = {
-        lon: uLong,
-        lat: uLat
-    }
-    console.log(userLocation);
 
-
+    var distanceVega = distance(userLocation.uLong, userLocation.uLat, 55.668104, 12.544605);
+    console.log(distanceVega.toFixed(2));
 }
 
-//Calculate distance to the concert venues out of the difference between the coordinates of the user's pre-selected location in km
-
-function distance() {
-    var lat1;
-    lat1 = userLocation.lon;
-    var lon1;
-    lon1 = userLocation.lat;
-    var lat2 = 55.668104;
-    var lon2 = 12.544605;
+function distance(lat1, lon1, lat2, lon2) {
     var radlat1 = Math.PI * lat1 / 180;
     var radlat2 = Math.PI * lat2 / 180;
     var radlon1 = Math.PI * lon1 / 180;
@@ -54,13 +33,11 @@ function distance() {
     dist = Math.acos(dist);
     dist = dist * 180 / Math.PI;
     dist = dist * 60 * 1.1515 * 1.609344;
-    console.log(dist.toFixed(2));
-    userDistanceVega = {
-        distanceVega: dist
-    }
-    console.log(userDistanceVega)
+
+    return dist;
 }
 
 
-
-
+document.getElementById("buttonClick").addEventListener("onchange", function () {
+    getCoords();
+});
