3ce66e3117df0bf3fd074e43b779a4c9c02cbcae
Chris Algier <chal18ac@student.cbs.dk>
2018-10-24 10:30:52 +0200

    Geolaction Feature

--- geoloc.js
+++ geoloc.js
@@ -4,35 +4,44 @@
     console.log (userL);
 }
 */
-var uLong = ""
-var uLat = ""
 
+//Get coordinates from user input and create an object for the user's location
 function getCoords() {
-    var userL = document.getElementById("locSelect").value
+    var userL = document.getElementById("locSelect").value;
     if ((userL === "Frederiksberg")) {
-        uLong = 55.676936 , uLat = 12.506579
+        uLong = 55.676936;
+        uLat = 12.506579;
         console.log(uLong, uLat)
 
     } else if ((userL === "Nørrebro")) {
-        uLong = 55.699031 , uLat = 12.556984
+        uLong = 55.699031;
+        uLat = 12.556984;
         console.log(uLong, uLat)
 
     } else if ((userL === "Vesterbro")) {
-        uLong = 55.664409 , uLat = 12.541514
+        uLong = 55.664409;
+        uLat = 12.541514;
         console.log(uLong, uLat);
 
     }
     document.getElementById("demo").innerHTML = userL;
     console.log(userL);
-    return {lon: uLong, lat: uLat};
+    userLocation = {
+        lon: uLong,
+        lat: uLat
+    }
+    console.log(userLocation);
+
 
 }
 
-function distanceVega(uLong, uLat, lat2, lon2) {
+//Calculate distance to the concert venues out of the difference between the coordinates of the user's pre-selected location in km
+
+function distance() {
     var lat1;
-    lat1 = getCoords.lon;
+    lat1 = userLocation.lon;
     var lon1;
-    lon1 = getCoords.lat;
+    lon1 = userLocation.lat;
     var lat2 = 55.668104;
     var lon2 = 12.544605;
     var radlat1 = Math.PI * lat1 / 180;
@@ -45,9 +54,13 @@ function distanceVega(uLong, uLat, lat2, lon2) {
     dist = Math.acos(dist);
     dist = dist * 180 / Math.PI;
     dist = dist * 60 * 1.1515 * 1.609344;
-    console.log(dist);
-    return dist
-
+    console.log(dist.toFixed(2));
+    userDistanceVega = {
+        distanceVega: dist
+    }
+    console.log(userDistanceVega)
 }
+
+
 
 
