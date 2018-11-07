c18f919f92cf7e88fa5345d7257d03354adc9dd5
sophianagler <43237937+sophianagler@users.noreply.github.com>
2018-11-07 12:37:28 +0100

    Create class.js

---
+++ class.js
@@ -0,0 +1,39 @@
+// Create class User
+class User {
+    constructor(firstName, surName, userName, eMail, password) {
+        this.firstName = firstName;
+        this.surName = surName;
+        this.userName = userName;
+        this.eMail = eMail;
+        this.password = password;
+    }
+}
+
+class Concert {
+    constructor(artistName, concertName, ticketPrice, venueName, concertGenre, concertDate, ticketCount, concertTime) {
+        this.artistName = artistName;
+        this.concertName = concertName;
+        this.ticketPrice = ticketPrice;
+        this.venueName = venueName;
+        this.concertGenre = concertGenre;
+        this.ticketCount = ticketCount;
+        this.concertTime = concertTime;
+        this.concertDate = concertDate;
+    }
+}
+
+class UserLocation {
+    constructor(userName, longitude, latitude) {
+        this.userName = userName;
+        this.longtitude = longitude;
+        this.latitude = latitude;
+    }
+}
+
+class Venue {
+    constructor(venueName, longitude, latitude) {
+        this.venueName = venueName;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+}
